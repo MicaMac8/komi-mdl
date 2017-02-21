6e3250c5bc2d6c4bbeb4afbd0665e69d2062ef64
Michael Maccio <m.michael.dev@gmail.com>
2017-02-21 18:09:24 +0100

    add linters

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 
 import './App.css'
 
-class App extends Component {
-  render() {
+import Page from '../presentation/Page'
+import Header from '../presentation/Header'
+
+export default class App extends Component {
+  render () {
     return (
-      <div className="App">
-        <h1>hello world</h1>
+      <div className='App'>
+        <Page>
+          <Header title='komi' />
+        </Page>
       </div>
     )
   }
 }
-
-export default App
---
+++ src/presentation/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+export default (props) => (
+  <header className='header-container'>
+    <h1>{props.title}</h1>
+  </header>
+)
